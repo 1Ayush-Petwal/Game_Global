fafbba28ebe31838686f14cfc28a1587253c3dd1
1Ayush-Petwal <ayushpetwal.0105@gmail.com>
2025-02-03 23:26:06 +0530

    first commit

---
+++ main/gui/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SignUp() {
+    return (
+        <>
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        </>
+    );
+}
